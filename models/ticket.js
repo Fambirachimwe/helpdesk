65b7be0c287545aae59de67eef5a11ad07b4fe47
Fambirachimwe <t.svaku@gmail.com>
2020-07-07 13:32:16 +0200

    added the status and priority schema methods

--- models/ticket.js
+++ models/ticket.js
@@ -16,34 +16,19 @@ const TicketSchema = new Schema({
     elevatedTo: {type: mongoose.Schema.Types.ObjectId, required:false}
 });
 
+
 // static methods to update the status and the priority
 
 TicketSchema.methods.updateStatus = async (status) => {
-    // take the sting of the status from the frontend dropdown
-    // search if the string exist
-    // return the id of the status string 
-    // upstate the status with the id and await save
-
-    const stId = async () => {
-        Status.find({ name: status }).then(data => {
-            return data._id
-        });
-    }
-
-    this.status = await stId;
+    // takes status from the frontend and updates the status string 
+    this.status = status;
     await this.save()
-
 }
 
 
 TicketSchema.methods.updatePriority = async (priority) => {
-    const prId = async () => {
-        Priority. find({name: priority }).then(data => {
-            return data._id;
-        });
-    }
-
-    this.priority = await prId;
+    // takes the priority from the frontend and updates the priority string
+    this.priority = priority;
     await this.save();
 }
 
